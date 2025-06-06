b6895d8875917447c458c136d7acf654c817e81f
zhangchark <zhangchark@gmail.com>
2025-06-06 10:48:44 +0800

    fix: renew the deprecated api BaseReactPackage in Android

--- android/src/main/java/com/solarengineanalysisreactnative/SolarengineAnalysisReactNativePackage.kt
+++ android/src/main/java/com/solarengineanalysisreactnative/SolarengineAnalysisReactNativePackage.kt
@@ -1,33 +1,16 @@
 package com.solarengineanalysisreactnative
 
-import com.facebook.react.BaseReactPackage
+import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.module.model.ReactModuleInfo
-import com.facebook.react.module.model.ReactModuleInfoProvider
-import java.util.HashMap
+import com.facebook.react.uimanager.ViewManager
 
-class SolarengineAnalysisReactNativePackage : BaseReactPackage() {
-  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
-    return if (name == SolarengineAnalysisReactNativeModule.NAME) {
-      SolarengineAnalysisReactNativeModule(reactContext)
-    } else {
-      null
-    }
+class SolarengineAnalysisReactNativePackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(SolarengineAnalysisReactNativeModule(reactContext))
   }
 
-  override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
-    return ReactModuleInfoProvider {
-      val moduleInfos: MutableMap<String, ReactModuleInfo> = HashMap()
-      moduleInfos[SolarengineAnalysisReactNativeModule.NAME] = ReactModuleInfo(
-        SolarengineAnalysisReactNativeModule.NAME,
-        SolarengineAnalysisReactNativeModule.NAME,
-        false,  // canOverrideExistingModule
-        false,  // needsEagerInit
-        false,  // isCxxModule
-        true // isTurboModule
-      )
-      moduleInfos
-    }
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
   }
 }
