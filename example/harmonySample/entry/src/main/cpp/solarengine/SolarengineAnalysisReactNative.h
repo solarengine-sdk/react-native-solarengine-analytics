56875df253e5ba8020cb48ecd4f2ef9cf2c7dba1
iMengqing <mengqing.gao@mintegral.com>
2026-06-12 14:27:55 +0800

    refactor(example): split harmony demo out of example and clean release workflow

--- example/harmonySample/entry/src/main/cpp/solarengine/SolarengineAnalysisReactNative.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "RNOH/ArkTSTurboModule.h"
-
-namespace rnoh {
-class JSI_EXPORT SolarengineAnalysisReactNative : public ArkTSTurboModule {
-  public:
-    SolarengineAnalysisReactNative(const ArkTSTurboModule::Context ctx, const std::string name);
-};
-} // namespace rnoh
