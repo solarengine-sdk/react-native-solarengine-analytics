c021bb317a628e2cf39c14ab7b6417d2ebbae09b
iMengqing <mengqing.gao@mintegral.com>
2026-09-11 19:42:20 +0800

    fix(harmony): align initialize metadata arity

--- harmony/cpp/solarengine/SolarengineAnalysisReactNative.cpp
+++ harmony/cpp/solarengine/SolarengineAnalysisReactNative.cpp
@@ -8,7 +8,7 @@ SolarengineAnalysisReactNative::SolarengineAnalysisReactNative(const ArkTSTurboM
     : ArkTSTurboModule(ctx, name) {
     methodMap_ = {
         ARK_METHOD_METADATA(preInit, 1),
-        ARK_METHOD_METADATA(initialize, 6),
+        ARK_METHOD_METADATA(initialize, 4),
         ARK_METHOD_METADATA(setReactNativeBridgeVersion, 1),
         ARK_METHOD_METADATA(multiply, 2),
 
